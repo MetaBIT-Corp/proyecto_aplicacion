767a7d6f358f12a5ebd296049a03f22ae5fce1d5
Pablo <diazcolato1997@gmail.com>
2019-04-17 21:34:38 -0600

    2019-04-17 21:34

--- app/src/main/java/com/example/crud_encuesta/GpoEmpActivity.java
+++ app/src/main/java/com/example/crud_encuesta/GpoEmpActivity.java
@@ -1,7 +1,12 @@
 package com.example.crud_encuesta;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -18,5 +23,26 @@ public class GpoEmpActivity extends AppCompatActivity {
         dao = new DaoGrupoEmp(this);
         lista_gpo_emp = dao.verTodos();
         adaptador = new Adaptador(lista_gpo_emp,this,dao);
+        ListView list = (ListView)findViewById(R.id.lista);
+        Button agregar = (Button)findViewById(R.id.btn_nuevo);
+        list.setAdapter(adaptador);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dialog dialog = new Dialog(GpoEmpActivity.this);
+                dialog.setTitle("Nuevo Grupo Emparejamiento");
+                dialog.setCancelable(true);
+                dialog.setContentView(R.layout.dialogo_gpo_emp);
+                dialog.show();
+            }
+        });
     }
 }
