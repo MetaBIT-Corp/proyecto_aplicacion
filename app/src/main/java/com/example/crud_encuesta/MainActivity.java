89faf72c478c12bb68980bd66333cf3a0cff76d2
Edwin Palacios <ap16014@ues.edu.sv>
2019-04-22 16:01:18 -0600

    editext tipo number

--- app/src/main/java/com/example/crud_encuesta/MainActivity.java
+++ app/src/main/java/com/example/crud_encuesta/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 //Login();
                 //clave();
-                pressed();
+                evaluacion();
 
 
             }
