e9d7bb58fbc6e2019e0e664a1129b4b442ead1c7
Edwin Palacios <ap16014@ues.edu.sv>
2019-05-10 00:15:13 -0600

    Se creo la clase DaoUsuario, que permite realizar los login y logout de usuarios, además de obtener el usuario logueado

--- app/src/main/java/com/example/crud_encuesta/Componentes_AP/Activities/LoginActivity.java
+++ app/src/main/java/com/example/crud_encuesta/Componentes_AP/Activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.crud_encuesta.Componentes_AP.Activities;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TextInputLayout;
@@ -9,7 +10,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.crud_encuesta.Componentes_AP.DAO.DAOUsuario;
+import com.example.crud_encuesta.Componentes_AP.Models.Usuario;
 import com.example.crud_encuesta.DatabaseAccess;
+import com.example.crud_encuesta.MainActivity;
 import com.example.crud_encuesta.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,55 +21,41 @@ public class LoginActivity extends AppCompatActivity {
     SQLiteDatabase baseDeDatos;
     TextInputLayout tvUsuario;
     TextInputLayout tvPass;
+    DAOUsuario daoUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        daoUsuario = new DAOUsuario(this);
 
         //enlazamos con el layout
         tvUsuario = (TextInputLayout) findViewById(R.id.ap_tiluser);
         tvPass = (TextInputLayout) findViewById(R.id.ap_tilpass);
         Button login = (Button) findViewById(R.id.ap_bt_login);
 
+
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(isUsuario()){
-                 //TODO:Intent a menu principal
+                //guardamos en variables los datos que ha ingresado el usuario
+                final String usuario = tvUsuario.getEditText().getText().toString();
+                final String pass = tvPass.getEditText().getText().toString();
+
+                if(daoUsuario.loginUsuario(pass,usuario)){
+                    Usuario usuarioLogueado = daoUsuario.getUsuarioLogueado();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("id_user",usuarioLogueado.getIDUSUARIO());
+                    intent.putExtra("rol_user",usuarioLogueado.getROL());
+                    startActivity(intent);
+                    Toast.makeText(v.getContext(),"Bienvenido " + usuario,Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(v.getContext(),"Usuario y/o contraseña no válidos",Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
     }
 
-    public Boolean isUsuario(){
-        Boolean isUser = Boolean.FALSE;
-        //guardamos en variables los datos que ha ingresado el usuario
-        String usuario = tvUsuario.getEditText().getText().toString();
-        String pass = tvPass.getEditText().getText().toString();
-
-        if(!usuario.isEmpty()&&!pass.isEmpty()){
-            //abrimos la base de datos
-            DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
-            baseDeDatos = databaseAccess.open();
-
-            //realizamos consulta   "Select * FROM USUARIO WHERE NOMUSUARIO= '" + usuario + "' AND CLAVE = '" +pass+"'"
-            Cursor cursor = baseDeDatos.rawQuery(
-                    "Select * FROM USUARIO WHERE NOMUSUARIO= '" + usuario + "' AND CLAVE = '" +pass+"'",
-                    null
-            );
-            //validamos
-            if(cursor.moveToFirst()){
-                isUser = Boolean.TRUE;
-                Toast.makeText(this,"Bienvenido " + usuario,Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(this,"Usuario y/o contraseña no válidos",Toast.LENGTH_SHORT).show();
-            }
-
-            //cerramos la base de datos
-            databaseAccess.close();
-        }else{
-            Toast.makeText(this,"Debe llenar los campos usuario y contraseña",Toast.LENGTH_SHORT).show();
-        }
-        return isUser;
-    }
 }
---
+++ app/src/main/java/com/example/crud_encuesta/Componentes_AP/DAO/DAOUsuario.java
@@ -0,0 +1,152 @@
+package com.example.crud_encuesta.Componentes_AP.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
+
+import com.example.crud_encuesta.Componentes_AP.Models.Turno;
+import com.example.crud_encuesta.Componentes_AP.Models.Usuario;
+import com.example.crud_encuesta.DatabaseAccess;
+
+import java.util.ArrayList;
+
+public class DAOUsuario {
+    SQLiteDatabase baseDeDatos;
+    Usuario usuario;
+    Context contexto;
+    String nombreBD= "proy_aplicacion";
+
+    //constructor
+    public DAOUsuario(Context context){
+        this.contexto = context;
+
+        DatabaseAccess dba = DatabaseAccess.getInstance(context);
+        baseDeDatos = dba.open();
+
+        /*
+         *Abrir base de datos
+         * DatabaseAccess dba = DatabaseAccess.getInstance(context);
+         * baseDeDatos = dba.open();
+         *
+         * */
+    }
+
+
+
+
+    //retorna true si se realiza el login con éxito
+    public Boolean loginUsuario(String pass, String user){
+        provisional();
+        Boolean login = false;
+        int id_usuario;
+        int id_sesion;
+        Cursor cursor = baseDeDatos.rawQuery(
+                "Select * FROM USUARIO WHERE NOMUSUARIO= '" + user + "' AND CLAVE = '" +pass+"'",
+                null
+        );
+        if(cursor.moveToFirst()){
+           id_usuario = cursor.getInt(0);
+           Cursor cursorSesion = baseDeDatos.rawQuery(
+                   "Select * FROM SESIONUSUARIO WHERE IDUSUARIO = "+ id_usuario,
+                   null
+           );
+           if (cursorSesion.moveToFirst()){
+               id_sesion = cursorSesion.getInt(0);
+               ContentValues contentValues = new ContentValues();
+               contentValues.put("INSESION", 1);
+               baseDeDatos.update("SESIONUSUARIO",contentValues,"IDSESION =" + id_sesion,null);
+               login = true;
+           }else{
+               Toast.makeText(contexto,
+                       "La sesión no existe",
+                       Toast.LENGTH_SHORT).show();
+           }
+        }else {
+            Toast.makeText(contexto,
+                    "El usuario no existe",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return login;
+    }
+
+    public Boolean logoutUsuario(int id_user){
+        Boolean logout = false;
+        int id_usuario;
+        int id_sesion;
+        Cursor cursor = baseDeDatos.rawQuery(
+                "Select * FROM USUARIO WHERE IDUSUARIO = " + id_user,
+                null
+        );
+        if(cursor.moveToFirst()){
+            id_usuario = cursor.getInt(0);
+            Cursor cursorSesion = baseDeDatos.rawQuery(
+                    "Select * FROM SESIONUSUARIO WHERE IDUSUARIO = "+ id_usuario,
+                    null
+            );
+            if (cursorSesion.moveToFirst()){
+                id_sesion = cursorSesion.getInt(0);
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("INSESION", 0);
+                baseDeDatos.update("SESIONUSUARIO",contentValues,"IDSESION =" + id_sesion,null);
+                logout = true;
+            }else{
+                Toast.makeText(contexto,
+                        "El usuario no tiene sesión",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(contexto,
+                    "Id de usuario no valido",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return logout;
+    }
+
+    //provisional
+    public void provisional(){
+        Cursor cursor = baseDeDatos.rawQuery(
+                "Select * FROM USUARIO",
+                null
+        );
+        if(cursor.moveToFirst()){
+            cursor.moveToFirst();
+            do{
+                logoutUsuario(cursor.getInt(0));
+            }while (cursor.moveToNext());
+        }
+    }
+
+    public Usuario getUsuarioLogueado(){
+        Usuario usuario = new Usuario();
+
+        Cursor cursorSesion = baseDeDatos.rawQuery(
+                "Select * FROM SESIONUSUARIO WHERE INSESION = "+ 1,
+                null
+        );
+        if (cursorSesion.moveToFirst()){
+            Cursor cursorUsuario = baseDeDatos.rawQuery(
+                    "Select * FROM USUARIO WHERE IDUSUARIO = "+ cursorSesion.getInt(2),
+                    null
+            );
+            if (cursorUsuario.moveToFirst()){
+                usuario.setIDUSUARIO(cursorUsuario.getInt(0));
+                usuario.setNOMUSUARIO(cursorUsuario.getString(1));
+                usuario.setCLAVE(cursorUsuario.getString(2));
+                usuario.setROL(cursorUsuario.getInt(3));
+                Toast.makeText(
+                        contexto,
+                        "Usuario: " +usuario.getNOMUSUARIO()+" Rol: " + usuario.getROL(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }else {
+                usuario = null;
+            }
+
+        }else {
+            usuario= null;
+        }
+        return usuario;
+    }
+}
